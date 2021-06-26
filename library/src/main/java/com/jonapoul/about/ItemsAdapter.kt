57456f856b1ab20aaf02ab5c3835a76578cffddb
jonapoul <jpoulton@protonmail.com>
2021-06-26 16:14:17 +0100

    Added auto-documentation  (#2)

--- library/src/main/java/com/jonapoul/about/ItemsAdapter.kt
+++ library/src/main/java/com/jonapoul/about/ItemsAdapter.kt
@@ -6,19 +6,28 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.jonapoul.extensions.hide
-import com.jonapoul.extensions.show
+import com.jonapoul.extensions.view.hide
+import com.jonapoul.extensions.view.show
 
+/**
+ * Inflates and initialises a list of [AboutItem]s for display.
+ */
 internal class ItemsAdapter(
     private val items: List<AboutItem>
 ) : RecyclerView.Adapter<ItemsAdapter.ViewHolder>() {
 
+    /**
+     * Inflates the [View]
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val root = inflater.inflate(R.layout.about_item, parent, false)
         return ViewHolder(root)
     }
 
+    /**
+     * Binds the [ViewHolder]
+     */
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items[position]
         holder.icon.setImageResource(item.icon)
@@ -30,13 +39,17 @@ internal class ItemsAdapter(
                 item.onClickButton.onItemClick(holder.itemView.context)
             }
         } else {
+            /* Remove the launcher button if no click listener is supplied */
             holder.launchButton.hide()
         }
     }
 
+    /**
+     * Number of items in the [RecyclerView]
+     */
     override fun getItemCount(): Int = items.size
 
-    class ViewHolder(root: View) : RecyclerView.ViewHolder(root) {
+    internal class ViewHolder(root: View) : RecyclerView.ViewHolder(root) {
         val icon: ImageView = root.findViewById(R.id.item_icon)
         val title: TextView = root.findViewById(R.id.item_title)
         val subtitle: TextView = root.findViewById(R.id.item_subtitle)
