44605039269ebf2cf0835c68beef82e5eea11e97
Jon Poulton <jpoulton@protonmail.com>
2021-06-30 21:08:35 +0100

    Added the ability to pull BuildConfig values via reflection

--- sample/src/main/java/com/jonapoul/about/sample/ExampleSections.kt
+++ sample/src/main/java/com/jonapoul/about/sample/ExampleSections.kt
@@ -1,53 +1,21 @@
 package com.jonapoul.about.sample
 
-import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import com.jonapoul.about.AboutItem
 import com.jonapoul.about.AboutSection
 
 object ExampleSections {
-    private const val GITHUB_ID = "jonapoul/about"
-    private const val REDDIT_SUB = "/r/androiddev"
-
     val BASIC = listOf(
-        AboutSection(
-            title = R.string.section_header_1,
-            items = listOf(
-                AboutItem.fromVersion(BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE),
-                AboutItem.fromBuildType(BuildConfig.BUILD_TYPE),
-                AboutItem.fromBuildTimeMs(BuildConfig.BUILD_TIME_MS),
-            )
+        AboutSection.fromBuildConfig(
+            buildConfig = BuildConfig::class.java,
+            buildTimeMsField = "BUILD_TIME_MS"
         ),
         AboutSection(
             title = R.string.section_header_2,
             items = listOf(
-                AboutItem(
-                    icon = R.drawable.ic_github,
-                    title = "GitHub",
-                    subtitle = GITHUB_ID,
-                    onClickButton = {
-                        openWebPage(context = it, url = "https://github.com/$GITHUB_ID")
-                    }
-                ),
-                AboutItem(
-                    icon = R.drawable.ic_reddit,
-                    title = "Reddit",
-                    subtitle = REDDIT_SUB,
-                    onClickButton = {
-                        openWebPage(context = it, url = "https://reddit.com$REDDIT_SUB")
-                    }
-                ),
+                AboutItem.fromGithub("https://github.com/jonapoul/about"),
+                AboutItem.fromReddit("https://reddit.com/r/androiddev"),
                 AboutItem.fromEmail("contact@company.org")
             )
         )
     )
-
-    private fun openWebPage(context: Context, url: String) {
-        /* Open a web browser to the page, or an app if installed */
-        val intent = Intent(Intent.ACTION_VIEW)
-        intent.data = Uri.parse(url)
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        context.startActivity(intent)
-    }
 }
